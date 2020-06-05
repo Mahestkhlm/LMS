749aa0d19edd043cbb0182fcd128a0e37f2ff092
Mahe <mahe22mca@gmail.com>
2020-06-05 14:35:44 +0200

    DB, ConfigureIdentity

--- LMSLexicon20/Data/ApplicationDbContext.cs
+++ LMSLexicon20/Data/ApplicationDbContext.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using LMSLexicon20.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LMSLexicon20.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User,IdentityRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        
     }
 }
