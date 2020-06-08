dcc7fdb5b2aa74e826158e19fd94582f3b4fac50
Josefin H <josefin.hallsson@gmail.com>
2020-06-08 15:36:25 +0200

    added CreateUserViewModel -> User mapping, added viewModel

--- LMSLexicon20/Data/MapperProfile.cs
+++ LMSLexicon20/Data/MapperProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using LMSLexicon20.Models;
+using LMSLexicon20.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,14 @@ namespace LMSLexicon20.Data
 {
     public class MapperProfile : Profile
     {
-
+        public MapperProfile()
+        {
+            //from viewmodel to user
+            CreateMap<CreateUserViewModel, User>()
+                .ForMember(dest => dest.UserName,
+                opt => opt.MapFrom(src => src.Email));
+                //.ForMember(dest => dest.CourseId,
+                //opt => opt.MapFrom(src => src.));
+        }
     }
 }
