cf348d8d1735ee11bdba8a34be1d8089b8b75cbc
Josefin H <josefin.hallsson@gmail.com>
2020-06-08 09:29:35 +0200

    Seed-setup in Program, Created roles in SeedData

---
+++ LMSLexicon20/Data/SeedData.cs
@@ -0,0 +1,36 @@
+﻿using LMSLexicon20.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMSLexicon20.Data
+{
+    public class SeedData
+    {
+        public static async Task InitializeAsync(IServiceProvider services)
+        {
+            var options = services.GetRequiredService<DbContextOptions<ApplicationDbContext>>();
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var userManager = services.GetRequiredService<UserManager<User>>();
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                var roleNames = new[] { "Teacher", "Student" }; //ToDo: ha med Member?
+
+                //--------SKAPA ROLLER--------
+                foreach (var name in roleNames)
+                {
+                    if (await roleManager.RoleExistsAsync(name)) continue;
+                    var role = new IdentityRole { Name = name };
+                    var result = await roleManager.CreateAsync(role);
+                    if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors));
+                }
+                //ToDo:SKAPA FÖRSTA TEACHER? 
+            }
+
+        }
+}
