0bee6867f8ab526ec45da45da6ee4d81ec2b1e3a
Mahe <mahe22mca@gmail.com>
2020-06-08 11:07:17 +0200

    fix

--- LMSLexicon20/ViewModel/CourseAddViewModel.cs
+++ LMSLexicon20/ViewModel/CourseAddViewModel.cs
@@ -9,30 +9,27 @@ namespace LMSLexicon20.ViewModel
     public class CourseAddViewModel
     {
 
-        public class CourseViewModel
-        {
-            //public int Id { get; set; }
+        //public int Id { get; set; }
 
-            [Required]
-            [Display(Name = "Course Name")]
-            public string Name { get; set; } //Course Name
+        [Required]
+        [Display(Name = "Course Name")]
+        public string Name { get; set; } //Course Name
 
 
-            [Required]
-            [Display(Name = "Start Date")]
-            [DataType(DataType.Date)]
-            public DateTime StartDate { get; set; }
+        [Required]
+        [Display(Name = "Start Date")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
 
-            [Required]
-            [Display(Name = "End Date")]
-            [DataType(DataType.Date)]
-            public DateTime EndDate { get; set; }
+        [Required]
+        [Display(Name = "End Date")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
 
-            [StringLength(200)]
-            public string Description { get; set; }
+        [StringLength(200)]
+        public string Description { get; set; }
 
-            
-        }
+
 
     }
 }
