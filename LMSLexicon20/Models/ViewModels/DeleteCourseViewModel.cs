1265e8a67403d92dd82f549c3dad5a2466e0d9ce
sladjantasic <sladjan.m.tasic@gmail.com>
2020-06-10 14:49:43 +0200

    DeleteCourse ViewModel

---
+++ LMSLexicon20/Models/ViewModels/DeleteCourseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMSLexicon20.Models.ViewModels
+{
+    public class DeleteCourseViewModel
+    {
+        [Display(Name = "Kursnamn")]
+        public string Name { get; set; }
+
+        [Display(Name = "Kursstart")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "Kursslut")]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Kursbeskrivning")]
+        public string Description { get; set; }
+
+        [Display(Name = "Lärare")]
+        public User Teacher { get; set; }
+
+        [Display(Name = "Studenter")]
+        public ICollection<User> Students { get; set; }
+    }
+}
