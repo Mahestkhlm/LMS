b3cb36f25d9cbcc98a41e039ba3f92d3de53e952
Mahe <mahe22mca@gmail.com>
2020-06-10 14:24:43 +0200

    CreateModule View Model

---
+++ LMSLexicon20/Models/ViewModels/CreateModuleViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMSLexicon20.Models.ViewModels
+{
+    public class CreateModuleViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Module Name")]
+        public string  Name { get; set; }
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+
+        public string Description { get; set; }
+        
+
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+        public ICollection<Document> Documents { get; set; }
+        public ICollection<Activity> Activities { get; set; }
+    }
+}
