2d55cb5a23a7963d52c46b2b64fcab0739de2fbe
Josefin H <josefin.hallsson@gmail.com>
2020-06-10 15:15:29 +0200

    created modulecontroller, created viewModel, created CreateModule-view (other views are scaffolded),

---
+++ LMSLexicon20/Models/ViewModels/CreateModuleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMSLexicon20.Models.ViewModels
+{
+    public class CreateModuleViewModel
+    {
+        [Required]
+        [Display(Name ="Namn")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Startdatum")]
+        public DateTime StartDate { get; set; }
+        [Required]
+        [Display(Name = "Slutdatum")]
+        public DateTime EndDate { get; set; }
+        [Display(Name = "Beskrivning")]
+        public string Description { get; set; }
+
+        //courseId?
+    }
+}
