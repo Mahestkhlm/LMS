a54e94233ba4650595c7f55d43e26d111911a39b
sladjantasic <sladjan.m.tasic@gmail.com>
2020-06-11 10:47:24 +0200

    User Details viewmodel

---
+++ LMSLexicon20/Models/ViewModels/UserDetailsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMSLexicon20.Models.ViewModels
+{
+    public class UserDetailsViewModel
+    {
+        [Display(Name = "Roll")]
+        public string Role { get; set; }
+
+        [Display(Name = "Förnamn")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Efternamn")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Registreringsdatum")]
+        public DateTime RegDate { get; set; }
+
+        [Display(Name = "Kurs")]
+        public Course Course { get; set; }
+    }
+}
