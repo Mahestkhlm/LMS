f9668956509640affc81c640ef5e35b1cdd35a21
sladjantasic <sladjan.m.tasic@gmail.com>
2020-06-12 10:43:08 +0200

    viewmodel att lägga lärare till kurs

---
+++ LMSLexicon20/Models/ViewModels/AddTeacherToCourseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMSLexicon20.Models.ViewModels
+{
+    public class AddTeacherToCourseViewModel
+    {
+        public string UserId { get; set; }
+    }
+}
