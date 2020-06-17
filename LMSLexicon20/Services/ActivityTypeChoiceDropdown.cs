1a892f5b4ee33ff0cc5c170d6aca20c6e0bf5b03
sladjantasic <sladjan.m.tasic@gmail.com>
2020-06-17 14:23:46 +0200

    actvity type dropdown

---
+++ LMSLexicon20/Services/ActivityTypeChoiceDropdown.cs
@@ -0,0 +1,24 @@
+﻿using LMSLexicon20.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMSLexicon20.Services
+{
+    public class ActivityTypeChoiceDropdown : IActivityTypeChoiceDropdown
+    {
+        private readonly ApplicationDbContext context;
+
+        public ActivityTypeChoiceDropdown(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<SelectListItem> GetSelectList()
+        {
+            return context.ActivityTypes.Select(e => new SelectListItem { Text = e.Name, Value = e.Id.ToString() });
+        }
+    }
+}
