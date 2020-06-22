d48b7fdf1f328e1175983950323870496e376c72
Mahe <mahe22mca@gmail.com>
2020-06-22 11:00:13 +0200

    Student Index+ user controller// aktuell vecka//Visa uppgifter

--- LMSLexicon20/Models/ViewModels/StudentIndexViewModel.cs
+++ LMSLexicon20/Models/ViewModels/StudentIndexViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,14 +12,38 @@ namespace LMSLexicon20.Models.ViewModels
         //public string Id { get; set; }
         [Display(Name = "Namn")]
         public string FullName { get; set; }
-        //public string Email { get; set; }
-
-        //[Display(Name = "Telefonnummer")]
-        //public string PhoneNumber { get; set; }
+        
         [Display(Name = "Kurs")]
         public Course Course { get; set; }
         public Activity Activity { get; set; }
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
         public ICollection <Activity>Activities { get; set; }
-        public ICollection<Document> Documents { get; set; }
+        public ICollection<Activity> Assignments { get; set; }
+  
+        public IEnumerable<Activity> CurrentActivites { get; set; }
+        public List<Activity> NextActivites { get; set; }
+        public List<Activity> PrevActivities { get; set; }
+        public IEnumerable<Activity> CurrentAssignments { get; set; }
+        public IEnumerable<Activity> LateAssignments { get; set; }
+
+
+
+
+
+
+        public ICollection<Activity> WeeklyActivities { get; set; }
+        public int GetWeekNumber()
+        {
+            DateTimeFormatInfo dfi = DateTimeFormatInfo.CurrentInfo;
+            DateTime date1 = DateTime.Now;
+            //DateTime date2 = new DateTime(2020, 06, 29); //yyyy, MM, dd
+            Calendar cal = dfi.Calendar;
+
+            return cal.GetWeekOfYear(date1, dfi.CalendarWeekRule, dfi.FirstDayOfWeek);
+        }
+        public string[] WeekDays { get { return new string[] { "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag" }; } set { } }
+        //public int CurrentWeek { get { return DateTime.Now.DayOfYear / 7; } set { } }
     }
+
 }
